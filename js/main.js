1a27fa30870b34cb57b71599f4e2ac87c7e6cff4
sujeonge <158864763+sujeonge@users.noreply.github.com>
2024-03-21 01:27:51 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,12 +1,32 @@
-var $circle = $('.cursor');
+// var $circle = $('.cursor');
 
-function moveCircle(e) {
-  TweenLite.to($circle, 0.3, {
-    css: {
-      left: e.pageX,
-      top: e.pageY
-    }
-  });
-}
+// function moveCircle(e) {
+//   TweenLite.to($circle, 0.3, {
+//     css: {
+//       left: e.pageX,
+//       top: e.pageY
+//     }
+//   });
+// }
 
-$(window).on('mousemove', moveCircle);
+// $(window).on('mousemove', moveCircle);
+
+// const content1 = document.querySelector('.content1')
+// const path1length = content1.getTotalLength()
+
+// content1.style.strokeDasharray = path1length
+// content1.style.strokeDashoffset = path1length
+
+// function calcDashoffset(scrollY, element, length){
+//   const ratio = (scrollY - element.offsetTop)/element.offsetHeight
+//   const value = length - (length * ratio)
+//   return value < 0 ? 0:value > length ? length : value
+// }
+
+// function scrollHandler(){
+//   const scrollY = window.scrollY + (window.innerHeight*0.1)
+//   content1.style.strokeDashoffset = calcDashoffset(scrollY,content1,path1length)
+// }
+
+// window.addEventListener('scroll', scrollHandler)
+
